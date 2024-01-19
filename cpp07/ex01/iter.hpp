56c40bd2b19b95e3d2de166b4719bc3ba20c41c9
Juan Antonio Domínguez <97060625+jdomingu98@users.noreply.github.com>
2024-01-19 16:49:53 +0100

    added iter implementation cpp02-ex01

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -11,9 +11,22 @@ void iter(T *array, int length, void (*f)(T const &))
 }
 
 template <typename T>
+void iter(T *array, int length, void (*f)(T &))
+{
+    for (int i = 0; i < length; i++)
+        f(array[i]);
+}
+
+template <typename T>
 void printNumberMultiplyByTen(T const &i)
 {
     std::cout << i * 10 << " ";
+}
+
+template <typename T>
+void printNumberMultiplyByTwo(T &i)
+{
+    std::cout << i * 2 << " ";
 }
 
 #endif
